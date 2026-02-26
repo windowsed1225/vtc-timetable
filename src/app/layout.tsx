f9f710fb538b1caaa881634fa99ef9f0296b5b32
windowsed <me@windowsed.me>
2026-02-27 01:01:03 +0800

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
+import Providers from "@/components/Providers";
 import type { Metadata } from "next";
 import "./globals.css";
-import Providers from "@/components/Providers";
 
 export const metadata: Metadata = {
   title: "VTC Calendar | Class Schedule",
   description:
     "View your VTC class schedule on a beautiful calendar and export it to your favorite calendar app.",
+  icons: {
+    icon: [
+      {
+        url: '/vtctimetable.svg', // Reference from the public directory
+        href: '/vtctimetable.svg',
+        type: 'image/svg+xml',
+        sizes: 'any', // Best practice for SVG icons
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
